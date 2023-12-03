77e50b4e0635660b0920a25075bf1321e0fc2368
Steven Sousa <silver.steven67@gmail.com>
2023-12-02 21:02:44 -0500

    Error handling for getting file open mode

--- file_opener.py
+++ file_opener.py
@@ -12,14 +12,14 @@ def get_file_name():
         filename = input('\nEnter a file name with its extension (ex: "file_name.txt") or\n'
                          'To EXIT in Windows, type ">q" or ">Q". In macOS, type ":Q" or ":q": ').lower()
 
-        if filename_is_valid(filename):
+        if check_if_filename_is_valid(filename):
             print("\nTarget file: ", filename)
             return filename
         else:
             print("Invalid filename. Please type a valid file name.")
 
 
-def filename_is_valid(filename):
+def check_if_filename_is_valid(filename):
     if filename.endswith(".txt"):
         return True
     elif filename in ['>q', ':q']:
@@ -29,17 +29,29 @@ def filename_is_valid(filename):
 
 
 def get_file_open_mode():
-    open_mode = input('\nFrom the list below, what mode would you like to open this file with?\n'
-                      '"r" - open for reading (default)\n'
-                      '"w" - open for writing, truncating the file first\n'
-                      '"x" - open for exclusive creation, failing if the file already exists\n'
-                      '"a" - open for writing, appending to the end of file if it exists\n'
-                      'To EXIT in Windows, type ">q" or ">Q". In macOS, type ":Q" or ":q": ').lower()
+    while True:
+        mode = input('\nFrom the list below, what mode would you like to open this file with?\n'
+                     '"r" - open for reading (default)\n'
+                     '"w" - open for writing, truncating the file first. **WARNING**: This will delete the '
+                     'contents of an existing file! \n'
+                     '"x" - open for exclusive creation, failing if the file already exists\n'
+                     '"a" - open for writing, appending to the end of file if it exists\n'
+                     'To EXIT in Windows, type ">q" or ">Q". In macOS, type ":Q" or ":q": ').lower()
 
-    print('\nSelected open mode: ', open_mode)
+        if check_if_open_mode_is_valid(mode):
+            print('\nSelected open mode: ', mode)
+            return mode
+        else:
+            print("Invalid mode. Please type a valid mode.")
 
-    if open_mode in ['>q', ':q']: exit('\nProgram is now exiting. Goodbye!')
-    return open_mode
+
+def check_if_open_mode_is_valid(mode):
+    if mode in ['r', 'w', 'x', 'a']:
+        return True
+    elif mode in ['>q', ':q']:
+        exit('\nProgram is now exiting. Goodbye!')
+    else:
+        return False
 
 
 file_name = get_file_name()
