064d5f7a8b9896b2e91fa558add4a95f214ff308
Steven Sousa <silver.steven67@gmail.com>
2023-12-04 08:50:39 -0500

    added module documentation

---
+++ export_txt_file.py
@@ -0,0 +1,7 @@
+"""
+This module will export the filtered data received by data_filter.py module into a text file.
+
+Author:Steven Sousa
+version 1.2
+release date - December 2023
+"""
---
+++ export_xlsm_file.py
@@ -0,0 +1,7 @@
+"""
+This module will export the filtered data received by data_filter.py module into an excel file.
+
+Author:Steven Sousa
+version 1.2
+release date - December 2023
+"""
