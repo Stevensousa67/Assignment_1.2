428e85094b67c022c35d322e651ca2d5af1b95d9
Steven Sousa <silver.steven67@gmail.com>
2023-12-04 07:32:45 -0500

    fixed module documentation main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 The purpose of this application is to filter a large data set from NASA. The target dataset is a collection of 45,716
 data entries. Each entry has information about a single meteorite landing on Earth. During runtime, the user
 will select if the data should be filtered by mass - and consequently set the upper and lower bounds of the mass filter
- - or by year the meteorite fell onto Earth.
+ - or by year the meteorite fell onto Earth. Finally, the user will be asked if they'd like the data to be displayed in
+ the Terminal, exported to a .txt file or .xlsm file.
 
 Author: Steven Sousa
 version: 1.2
